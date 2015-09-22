b0dfbbdbee064f8a356221c722d7595432e08554
Yu Hoshino <yhoshino11@gmail.com>
2015-09-22 23:32:37 +0900

    Finish 20 - minimal server

---
+++ 20-inets.exs
@@ -0,0 +1,25 @@
+require Record
+
+defmodule WebServer do
+  def run do
+    :inets.start()
+    options = [server_name: 'foo', server_root: '/tmp', document_root: '/tmp', port: 3000, modules: [App]]
+    {:ok, _pid} = :inets.start :httpd, options
+    IO.puts "running on port 3000"
+    receive do: (_ -> :ok)
+  end
+end
+
+defmodule App do
+  Record.defrecord :mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
+
+  def unquote(:do)(data) do
+    response = case mod(data, :request_uri) do
+      '/' -> 'hello world'
+      _   -> ['hello ', mod(data, :request_uri)]
+    end
+    {:proceed, [response: {200, response}]}
+  end
+end
+
+WebServer.run
