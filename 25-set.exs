517583dd6a7e8138aa914ae7af7a55701bd4dfb6
Yu Hoshino <yhoshino11@gmail.com>
2015-09-23 00:53:03 +0900

    Finish 25 - Set

---
+++ 25-set.exs
@@ -0,0 +1,24 @@
+ExUnit.start
+
+defmodule SetTest do
+  use ExUnit.Case
+
+  test 'to_list' do
+    list = Enum.into([1, 2, 2, 3], HashSet.new) |> HashSet.to_list
+    assert list == [2, 3, 1]
+  end
+
+  test 'union' do
+    union = Enum.into([1, 2, 3], HashSet.new) |> HashSet.union(Enum.into([2, 3, 4], HashSet.new))
+    assert Set.to_list(union) == [2, 3, 4, 1]
+  end
+
+  test 'intersection' do
+    union = Enum.into([1, 2, 3], HashSet.new) |> HashSet.intersection(Enum.into([2, 3, 4], HashSet.new))
+    assert Set.to_list(union) == [2, 3]
+  end
+
+  test 'member?' do
+    refute Enum.into([1, 3, 5], HashSet.new) |> HashSet.member?(2)
+  end
+end
